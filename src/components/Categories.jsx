27298092a75cf044629ef46b84be7c39d8738288
Marina Kilbas <margog1982@gmail.com>
2026-06-20 17:38:13 +0300

    використовуємо useState

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react"
 
 const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  const categories = ["Всі", "М'ясi", "Вегетаріанські", "Гриль", "Гострі", "Закриті"]
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index)
+  }
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {
+          categories.map((categori, i) => (
+            <li key={i} className={activeIndex === i ? "active" : ""} onClick={() => onClickCategory(i)}>{categori}</li>
+          ))
+        }
       </ul>
     </div>
   )
