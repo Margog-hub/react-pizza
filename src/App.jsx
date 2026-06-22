928bc9164d515ef09f60702e8af6353bfe58ce70
Marina Kilbas <margog1982@gmail.com>
2026-06-22 16:15:29 +0300

    Підключаємо React Router v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,24 @@
-// import pizzas from './assets/pizzas.json'
-import Categories from './components/Categories';
-import Header from './components/Header';
-import PizzaBlock from './components/PizzaBlock';
-import Sort from './components/Sort';
+import { Routes, Route } from "react-router-dom";
 import './scss/app.scss';
-import { useEffect, useState } from 'react';
-import Skeleton from './components/PizzaBlock/Skeleton';
+import Header from './components/Header';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Cart from './pages/Cart';
+
+
 
 function App() {
-  const [pizzas, setPizzas] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    fetch('https://68750ca8dd06792b9c967d62.mockapi.io/item')
-      .then(res => { return res.json() })
-      .then(arr => {
-         setPizzas(arr)
-         setIsLoading(false) 
-        })
-  }, [])
-
-
   return (
     <>
       <div className="wrapper">
         <Header />
         <div className="content">
           <div className="container">
-            <div className="content__top">
-              <Categories />
-              <Sort />
-            </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-              {
-                isLoading ?
-                  [new Array(12)].map((_, index) => <Skeleton key={index} />) :
-                  pizzas.map((obj) => (<PizzaBlock key={obj.id} {...obj} />))
-              }
-            </div>
+            <Routes >
+              <Route path='/' element={< Home />} />
+              <Route path='/cart' element={< Cart />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
           </div>
         </div>
       </div>
