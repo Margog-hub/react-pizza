cc15104c42c749a56e21ca890b1afed67ec7120d
Marina Kilbas <margog1982@gmail.com>
2026-06-25 19:57:27 +0300

    зроблено функціонал сотрування та фільтрацію піц

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,20 +1,19 @@
-import { useState } from "react"
-
-const Categories = () => {
-  const [activeIndex, setActiveIndex] = useState(0)
+const Categories = ({ categoryId, onClickСategoryId }) => {
 
   const categories = ["Всі", "М'ясi", "Вегетаріанські", "Гриль", "Гострі", "Закриті"]
 
-  const onClickCategory = (index) => {
-    setActiveIndex(index)
-  }
 
   return (
     <div className="categories">
       <ul>
         {
-          categories.map((categori, i) => (
-            <li key={i} className={activeIndex === i ? "active" : ""} onClick={() => onClickCategory(i)}>{categori}</li>
+          categories.map((categoryName, i) => (
+            <li
+              key={i}
+              className={categoryId === i ? "active" : ""}
+              onClick={() => onClickСategoryId(i)}>
+              {categoryName}
+            </li>
           ))
         }
       </ul>
