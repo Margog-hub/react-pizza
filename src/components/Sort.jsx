49eb4ec0c9625ea3a5117fd7c48a62f9f0b3dd6c
Marina Kilbas <margog1982@gmail.com>
2026-07-11 16:01:54 +0300

    встановлення Redux Tooolkit

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setSort } from "../redux/slices/filterSlice";
 
-const Sort = ({ sortType, setSortType }) => {
+const Sort = () => {
+  const dispatch = useDispatch()
+  const sort = useSelector(state => state.filter.sort)
 
   const lists = [
     { name: 'популярністю(DESC)', sortProperty: 'rating' },
@@ -14,7 +18,7 @@ const Sort = ({ sortType, setSortType }) => {
   const [open, setOpen] = useState(false)
 
   const onClickListItem = (list) => {
-    setSortType(list)
+    dispatch(setSort(list))
     setOpen(false)
   }
 
@@ -25,7 +29,7 @@ const Sort = ({ sortType, setSortType }) => {
           <path d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z" fill="#2C2C2C"></path>
         </svg>
         <b>Сортувати за:</b>
-        <span >{sortType.name}</span>
+        <span >{sort.name}</span>
       </div>
       {
         open && <div className="sort__popup"> 
@@ -35,7 +39,7 @@ const Sort = ({ sortType, setSortType }) => {
                 <li
                   key={i}
                   onClick={() => onClickListItem(list)}
-                  className={sortType.sortProperty === list.sortProperty ? "active" : ''}>
+                  className={sort.sortProperty === list.sortProperty ? "active" : ''}>
                   {list.name}
                 </li>
               ))
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
+import { store } from './redux/store.jsx';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
+  <Provider store ={store}>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>
 )
